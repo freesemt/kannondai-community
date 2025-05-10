13c27692a1dbf09669fa08a1ae4afc3e0fad1bc7
freesemt <freesemt@gmail.com>
2025-05-10 22:11:08 +0900

    Update for better wording

--- docs/scripts/contact.js
+++ docs/scripts/contact.js
@@ -5,7 +5,7 @@ const encodedEmail = "ZnJlZXNlbXQra2Fubm9uX3F1ZXJ5QGdtYWlsLmNvbQ==";
 document.getElementById("contactButton").addEventListener("click", function () {
   const email = atob(encodedEmail); // Base64 をデコード
   const subject = encodeURIComponent("観音台の集会所・羽成公園のページについて"); // 件名を設定
-  const body = encodeURIComponent("意見・提案・質問の内容：\n\n\n"); // 本文を設定
+  const body = encodeURIComponent("有志連絡係さんへ\n\n意見・提案・質問の内容：\n\n\n"); // 本文を設定
   const timestamp = new Date().getTime(); // 現在のタイムスタンプを取得
   window.location.href = `mailto:${email}?subject=${subject}&body=${body}&t=${timestamp}`; // クエリパラメータを追加
 });
