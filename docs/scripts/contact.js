f5e37e2c6c02a410dfe94759fcd047dc2ef0a9f5
freesemt <freesemt@gmail.com>
2025-05-17 21:34:52 +0900

    Change contact address

--- docs/scripts/contact.js
+++ docs/scripts/contact.js
@@ -1,11 +1,11 @@
 // メールアドレスを Base64 エンコード
-const encodedEmail = "ZnJlZXNlbXQra2Fubm9uX3F1ZXJ5QGdtYWlsLmNvbQ==";
+const encodedEmail = "ZnJlZXNlbXQra2Fubm9uX2NvbnRhY3RAZ21haWwuY29t";
 
 // ボタンのクリックイベントでメールクライアントを起動
 document.getElementById("contactButton").addEventListener("click", function () {
   const email = atob(encodedEmail); // Base64 をデコード
   const subject = encodeURIComponent("観音台の羽成公園・集会所のページについて"); // 件名を設定
-  const body = encodeURIComponent("有志連絡係さんへ\n\n意見・提案・質問の内容：\n\n\n"); // 本文を設定
+  const body = encodeURIComponent("サイトの連絡係さんへ\n\n意見・提案・質問の内容：\n\n\n"); // 本文を設定
   const timestamp = new Date().getTime(); // 現在のタイムスタンプを取得
   window.location.href = `mailto:${email}?subject=${subject}&body=${body}&t=${timestamp}`; // クエリパラメータを追加
 });
